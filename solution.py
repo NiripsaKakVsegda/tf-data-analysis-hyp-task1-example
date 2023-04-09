9ca5e2fbf3096add5b7d1602eb91bad3a3212854
NiripsaKakVsegda <anna.poluyanenko@gmail.com>
2023-04-10 02:55:12 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,8 +12,8 @@ def solution(x_success: int,
     alpha = 0.05
     p1 = x_success / x_cnt
     p2 = y_success / y_cnt
-    err1 = sqrt(p1*(1-p1)/x_cnt)
-    err2 = sqrt(p2*(1-p2)/y_cnt)
-    z_score = (p2 - p1) / sqrt(err1**2+err2**2)
+    err1 = p1*(1-p1)/x_cnt
+    err2 = p2*(1-p2)/y_cnt
+    z_score = (p2 - p1) / sqrt(err1+err2)
     t_critical = stats.t.ppf(1-alpha/2, x_cnt+y_cnt-2)
     return z_score > t_critical
